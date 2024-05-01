a998b71305c50698656fe156b86071ae41d69047
realFrankieWilson <frankuwill101@gmail.com>
2024-05-01 12:33:15 +0100

    FEAT: started implementing the responsivness of the app

--- playpal/static/js/script.js
+++ playpal/static/js/script.js
@@ -1,1 +1,13 @@
-// alert('hello world');
+// A sellector function.
+
+document.addEventListener('DOMContentLoaded', function () {
+  //Grabs elements
+  const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if (element) return element;
+    throw new Error(`Sorry, that wasn't right, there is no \
+    element by the name "{selector}", Please check your spelling and try again!`);
+  };
+
+  console.log(selectElement('.hello'));
+})
