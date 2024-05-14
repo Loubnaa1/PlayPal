33fd84f0181bcd72b4441b1a097e40963130dfa0
realFrankieWilson <frankuwill101@gmail.com>
2024-05-14 19:10:23 +0100

    feat: Add follow functionality and profile edit page
    - Users can now follow each other
    - Added a profile edit page where users can update their profile information (name, language, etc.)
    - Updated the profile view to enhance user interactions

--- playpal/users/form.py
+++ playpal/users/form.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import ProfileModel
+from django.views.generic.edit import UpdateView
 
 
 class SignUpForm(UserCreationForm):
@@ -42,3 +43,4 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = ProfileModel
         fields = ["cover_image", "profile_image"]
+
--- playpal/users/urls.py
+++ playpal/users/urls.py
@@ -2,19 +2,47 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+from users.views import ProfileView, ProfileEditView, AddFollowers, RemoveFollower
+
 app_name = "users"
 urlpatterns = [
     path("sign_up/", views.sign_up, name="sign-up"),
-    path("profile/", views.user_profile, name="profile"),
+    path("profile/<int:pk>/", ProfileView.as_view(), name="profile"),
+    path("profile/<int:pk>/edit/", ProfileEditView.as_view(), name="profile-edit"),
+    path("profile/<int:pk>/follower/add", AddFollowers.as_view(), name="add-follower"),
+    path("profile/<int:pk>/follower/remove", RemoveFollower.as_view(), name="remove-follower"),
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="users/login.html"),
         name="login",
     ),
     path("logout/", views.logout_view, name="logout"),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="landing/password_reset.html"), name="password_reset"),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="landing/password_reset_done.html"), name="password_reset_done"),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="landing/password_reset_confirm.html"), name="password_reset_confirm"),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="landing/password_reset_complete.html"), name="password_reset_complete"),
-
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="landing/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset_done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="landing/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset_confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="landing/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password_reset_complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="landing/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
